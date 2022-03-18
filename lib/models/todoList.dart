902a5e57e32870d9d097306ef30afa71e3433125
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-18 23:37:41 +0530

    Made todoList datatype

---
+++ lib/models/todoList.dart
@@ -0,0 +1,8 @@
+class TodoList {
+  late final String id;
+  late final DateTime creation;
+  late final DateTime updation;
+  late final String type;
+  late final String title;
+  late final List<Map> list;
+}
