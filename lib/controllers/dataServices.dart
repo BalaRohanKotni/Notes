bc7c19a03bd51196193d41a9ec5853bb4e2c1f82
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-20 23:24:32 +0530

    Renamed functions in dataServices.dart

--- lib/controllers/dataServices.dart
+++ lib/controllers/dataServices.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: file_names
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> addData(id, data) async {
+Future<void> addDoc(id, data) async {
   const String collectionName = "storage";
   await FirebaseFirestore.instance.collection(collectionName).doc(id).set(data);
 }
 
-Future<void> updateData(id, dataToChange) async {
+Future<void> updateDoc(id, dataToChange) async {
   // nesting objects are also supported: "'a.b.c': 'z'"
   const String collectionName = "storage";
   await FirebaseFirestore.instance
@@ -17,7 +17,7 @@ Future<void> updateData(id, dataToChange) async {
       .catchError((error) => print("Failed to update: $error"));
 }
 
-Future<dynamic> readData(id) async {
+Future<dynamic> readDoc(id) async {
   const String collectionName = "storage";
   return await FirebaseFirestore.instance
       .collection(collectionName)
