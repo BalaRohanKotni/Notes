93e9f0e18cea056b66bc523bc45d509520f4f406
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-21 15:38:31 +0530

    Added toStr() to note.dart

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -6,11 +6,16 @@ class Note {
   late final String title;
   late final String body;
 
-  Note(
-      {this.id = "",
-      required this.creation,
-      required this.updation,
-      required this.type,
-      required this.title,
-      required this.body});
+  Note({
+    this.id = "",
+    required this.creation,
+    required this.updation,
+    required this.type,
+    required this.title,
+    required this.body,
+  });
+
+  toStr() {
+    return "ID: $id:\nCreation: $creation, Updation: $updation\n$title\n$body";
+  }
 }
