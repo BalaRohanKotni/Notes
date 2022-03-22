559e52ee4b7cc2c313f86998bdccfda71995f30f
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-22 23:25:40 +0530

    Updated path of docs in dataServices.dart

--- lib/controllers/dataServices.dart
+++ lib/controllers/dataServices.dart
@@ -3,13 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:notes/models/todoList.dart';
 
 Future<void> addDoc(data) async {
-  const String collectionName = "storage";
+  const String collectionName = "storage/users/";
   await FirebaseFirestore.instance.collection(collectionName).add(data);
 }
 
 Future<void> updateDoc(id, dataToChange) async {
   // nesting objects are also supported: "'a.b.c': 'z'"
-  const String collectionName = "storage";
+  const String collectionName = "storage/users";
   await FirebaseFirestore.instance
       .collection(collectionName)
       .doc(id)
@@ -19,7 +19,7 @@ Future<void> updateDoc(id, dataToChange) async {
 }
 
 Future<TodoList> readDoc(id) async {
-  const String collectionName = "storage";
+  const String collectionName = "storage/users";
   var doc = await FirebaseFirestore.instance
       .collection(collectionName)
       .doc(id)
@@ -40,7 +40,7 @@ Future<TodoList> readDoc(id) async {
 }
 
 Future<void> deleteDoc(id) async {
-  const String collectionName = "storage";
+  const String collectionName = "storage/users";
   await FirebaseFirestore.instance
       .collection(collectionName)
       .doc(id)
