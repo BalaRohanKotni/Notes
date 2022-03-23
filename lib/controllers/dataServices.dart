f0f4a426491a3a6f1f2d6cf29081722f56d2fae0
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-23 23:25:02 +0530

    Made changes to dataServices.dart to store data separately bw users

--- lib/controllers/dataServices.dart
+++ lib/controllers/dataServices.dart
@@ -1,48 +1,67 @@
 // ignore_for_file: file_names
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes/models/note.dart';
 import 'package:notes/models/todoList.dart';
 
 Future<void> addDoc(uid, data) async {
-  String collectionName = "storage/users/user/$uid";
-  await FirebaseFirestore.instance.collection(collectionName).add(data);
+  await FirebaseFirestore.instance
+      .collection('users')
+      .doc(uid)
+      .collection(data['type'])
+      .add(data);
 }
 
-Future<void> updateDoc(uid, id, dataToChange) async {
+Future<void> updateDoc(uid, id, dataToChange, type) async {
   // nesting objects are also supported: "'a.b.c': 'z'"
-  String collectionName = "storage/users/user/$uid";
   await FirebaseFirestore.instance
-      .collection(collectionName)
+      .collection('users')
+      .doc(uid)
+      .collection(type)
       .doc(id)
       .update(dataToChange)
       .then((value) => print("Changed"))
       .catchError((error) => print("Failed to update: $error"));
 }
 
-Future<TodoList> readDoc(uid, id) async {
-  String collectionName = "storage/users/user/$uid";
+Future<dynamic> readDoc(uid, id, dType) async {
   var doc = await FirebaseFirestore.instance
-      .collection(collectionName)
+      .collection('users')
+      .doc(uid)
+      .collection(dType)
       .doc(id)
       .get()
       .catchError((e) => print("Caught error: $e"));
 
-  TodoList list = TodoList(
-    creation:
-        DateTime.fromMillisecondsSinceEpoch(doc['creation'].seconds * 1000),
-    updation:
-        DateTime.fromMillisecondsSinceEpoch(doc['updation'].seconds * 1000),
-    type: doc['type'],
-    title: doc['title'],
-    list: doc['list'],
-  );
+  if (dType == 'list') {
+    TodoList list = TodoList(
+      creation:
+          DateTime.fromMillisecondsSinceEpoch(doc['creation'].seconds * 1000),
+      updation:
+          DateTime.fromMillisecondsSinceEpoch(doc['updation'].seconds * 1000),
+      type: doc['type'],
+      title: doc['title'],
+      list: doc['list'],
+    );
 
-  return list;
+    return list;
+  } else {
+    Note note = Note(
+        creation:
+            DateTime.fromMillisecondsSinceEpoch(doc['creation'].seconds * 1000),
+        updation:
+            DateTime.fromMillisecondsSinceEpoch(doc['updation'].seconds * 1000),
+        type: doc['type'],
+        title: doc['title'],
+        body: doc['body']);
+    return note;
+  }
 }
 
-Future<void> deleteDoc(uid, id) async {
-  String collectionName = "storage/users/user/$uid";
+Future<void> deleteDoc(uid, id, type) async {
   await FirebaseFirestore.instance
-      .collection(collectionName)
+      .collection('users')
+      .doc(uid)
+      .collection(type)
       .doc(id)
       .delete()
       .catchError((e) => print("Caught error: $e"));
