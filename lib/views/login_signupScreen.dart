68c7fefb654718480a2d42f709224df87cc1dcee
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-24 22:24:16 +0530

    Created login_signupScreen.dart

---
+++ lib/views/login_signupScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupScreen extends StatefulWidget {
+  const LoginSignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginSignupScreen> createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
