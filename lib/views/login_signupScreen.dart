6a9a1fb32be03ca8c0ad80b831fe48738d0323f0
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-24 23:33:40 +0530

    Made skeleton of login/signup card in login_signupScreen.dart

--- lib/views/login_signupScreen.dart
+++ lib/views/login_signupScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/constants.dart';
 
 class LoginSignupScreen extends StatefulWidget {
   const LoginSignupScreen({Key? key}) : super(key: key);
@@ -10,6 +11,50 @@ class LoginSignupScreen extends StatefulWidget {
 class _LoginSignupScreenState extends State<LoginSignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Container(
+        color: appTheme,
+        child: Container(
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(vertical: 225, horizontal: 40),
+          child: DefaultTabController(
+            length: 2,
+            child: Column(
+              children: const [
+                // TODO: Customize TabBar
+                TabBar(
+                  tabs: [
+                    Text(
+                      "Login",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+
+                // TODO: Customize TabBarView and data to it
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      Text(
+                        "Login",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
