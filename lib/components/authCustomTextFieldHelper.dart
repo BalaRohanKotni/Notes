a6af5234c2144819543751a814f9296a0e930f05
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-26 01:35:32 +0530

    Added CustomTextFeildHelper for auth screen

---
+++ lib/components/authCustomTextFieldHelper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Container AuthCustomTextFeildHelper(Text text) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+    alignment: Alignment.centerLeft,
+    child: text,
+  );
+}
