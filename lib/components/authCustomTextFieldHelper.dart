49a28b24a05783ff35ebaf4d66ef5440cc8da53f
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-29 01:07:04 +0530

    Deleted some unnecessary files

--- lib/components/authCustomTextFieldHelper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Container AuthCustomTextFeildHelper(Text text) {
-  return Container(
-    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-    alignment: Alignment.centerLeft,
-    child: text,
-  );
-}
