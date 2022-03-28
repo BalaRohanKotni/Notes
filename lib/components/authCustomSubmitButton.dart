b60e1a561d9ef9901febd007e443992ddfa1dcce
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-29 01:01:51 +0530

    Updated authCustomSubmitButton.dart

--- lib/components/authCustomSubmitButton.dart
+++ lib/components/authCustomSubmitButton.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
-Container AuthCustomSubmitButtons(Text text, Color color) {
+Container AuthCustomSubmitButtons(Text text, onPressed, beginColor, endColor) {
   return Container(
     margin: EdgeInsets.symmetric(horizontal: 60, vertical: 0),
     decoration: BoxDecoration(
       // color: Color(0xFF23CE6B),
-      color: color,
+      gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [beginColor, endColor],
+      ),
       borderRadius: BorderRadius.all(
         Radius.circular(50),
       ),
     ),
     width: double.infinity,
     child: TextButton(
-      onPressed: () {},
+      onPressed: onPressed,
       child: text,
     ),
   );
