5bbf9549bffd15ca4dc1e7955016e2f07a03afe3
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-29 01:03:27 +0530

    Updated login_signupScreen.dart

--- lib/views/login_signupScreen.dart
+++ lib/views/login_signupScreen.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:notes/constants.dart';
-
-import '../components/AuthCustomTextFeild.dart';
-import '../components/authCustomSubmitButton.dart';
-import '../components/authCustomTextFieldHelper.dart';
+import 'package:notes/views/loginScreen.dart';
+import '../components/login_signupChoiceButton.dart';
 
 class LoginSignupScreen extends StatefulWidget {
   const LoginSignupScreen({Key? key}) : super(key: key);
@@ -16,114 +13,32 @@ class LoginSignupScreen extends StatefulWidget {
 }
 
 class _LoginSignupScreenState extends State<LoginSignupScreen> {
-  late TabController _tabController;
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        // color: Color(0xFF23CE6B),
-        color: Color(0xFF272D2D),
+    return Scaffold(
+      body: Center(
         child: Container(
-          // color: Colors.white,
-          margin: const EdgeInsets.symmetric(vertical: 225, horizontal: 40),
-          child: DefaultTabController(
-            length: 2,
-            child: Column(
-              children: [
-                // TODO: Customize TabBar
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15)),
-                    color: Color(0xFF23CE6B),
+          height: double.infinity,
+          width: double.infinity,
+          color: kLoginSignupBGColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              loginSignupChoiceButton("Log in", () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
                   ),
-                  child: TabBar(
-                    indicatorColor: Color(0xFFA846A0),
-                    // indicator: BoxDecoration(color: Color(0xFF23CE6B)),
-                    tabs: [
-                      Container(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontFamily: 'SourceSansPro'),
-                        ),
-                      ),
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontFamily: 'SourceSansPro'),
-                      ),
-                    ],
-                  ),
-                ),
-
-                // TODO: Customize TabBarView and data to it
-                Expanded(
-                  child: Container(
-                    color: Color(0xFFF6F8FF),
-                    child: TabBarView(
-                      children: [
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 20,
-                            ),
-                            AuthCustomTextFeildHelper(
-                              Text(
-                                "Email",
-                                style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontFamily: "SourceSansPro",
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ),
-                            AuthCustomTextFeild(
-                                type: TextInputType.emailAddress),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            AuthCustomTextFeildHelper(Text(
-                              "Password:",
-                              style: TextStyle(
-                                  fontSize: 20.0,
-                                  fontFamily: "SourceSansPro",
-                                  fontWeight: FontWeight.w500),
-                            )),
-                            AuthCustomTextFeild(
-                                type: TextInputType.visiblePassword,
-                                obsureText: true),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            AuthCustomSubmitButtons(
-                              Text(
-                                "Login",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontFamily: "SourceSansPro",
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        Text(
-                          'Sign Up',
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+                );
+              }, 'login'),
+              const SizedBox(
+                height: 5,
+              ),
+              loginSignupChoiceButton("Sign up", () {
+                // TODO: Push screen to sign up screen
+              }, 'signUp'),
+            ],
           ),
         ),
       ),
