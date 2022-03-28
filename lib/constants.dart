914262b7aa969ea6782d7bc3cb917db4124dc301
BalaRohanKotni <balarohan.k@gmail.com>
2022-03-28 13:09:46 +0530

    Updated constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-final Color appTheme = Color(0xFF2F4368);
+const Color appTheme = Color(0xFF2F4368);
+
+const Color kLoginSignupBGColor = Color(0xFF101010);
+
+const Color kLoginSignupTextFeildOutlineColor = Color(0xFF672967);
+const Color kLoginSignupTextFeildFillColor = Color(0xFF171717);
+
+const Color kLoginSignupSubmitButonStartColor = Color(0xFFac2bcb);
+const Color kLoginSignupSubmitButonEndColor = Color(0xFFd133a6);
