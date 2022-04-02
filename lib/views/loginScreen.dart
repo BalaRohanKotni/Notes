586965e311b6b7e8b886749a9b0ec189c5c1c94a
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-03 01:25:35 +0530

    Updated loginScreen.dart

--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notes/components/authCustomTextField.dart';
 import 'package:notes/components/authCustomSubmitButton.dart';
 import 'package:notes/constants.dart';
+import 'package:notes/views/overview_screen.dart';
 import 'package:notes/views/signUpScreen.dart';
+import '../controllers/auth.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,96 +14,122 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: kLoginSignupBGColor,
+      color: Colors.white,
       child: SafeArea(
-        child: Scaffold(
-          backgroundColor: kLoginSignupBGColor,
-          appBar: AppBar(
-            backgroundColor: kLoginSignupBGColor,
-            title: const Hero(
-              tag: "login",
-              child: Text(
-                "Log in",
-                style: TextStyle(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 25),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              leading: GestureDetector(
+                child: Icon(
+                  Icons.arrow_back_rounded,
+                  color: kCeruleanBlue,
+                  size: 35,
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              leadingWidth: 45,
+              elevation: 0,
+              iconTheme: const IconThemeData(
+                color: kCeruleanBlue,
+              ),
+              backgroundColor: Colors.white,
+              title: const Hero(
+                tag: "login",
+                child: Text(
+                  "Log in",
+                  style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 30,
-                    color: Colors.white),
+                    fontFamily: 'LobsterTwo',
+                    fontSize: 40,
+                    color: kCaledonBLue,
+                  ),
+                ),
               ),
             ),
-          ),
-          body: Container(
-            margin: const EdgeInsets.all(25),
-            color: kLoginSignupBGColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // AuthCustomTextFeildHelper(Text(
-                //   "Email",
-                //   style: authTextFieldHelperTextStyle(),
-                // )),
-                AuthCustomTextFeild("Enter your email"),
-                const SizedBox(height: 20),
-                AuthCustomTextFeild("Enter your password",
-                    type: TextInputType.visiblePassword, obsureText: true),
-                const SizedBox(
-                  height: 25,
-                ),
-                AuthCustomSubmitButtons(
-                  const Text(
-                    "Submit",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "SourceSansPro",
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+            body: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AuthCustomTextFeild("Email", emailController),
+                  const SizedBox(height: 20),
+                  AuthCustomTextFeild("Password", pwdController,
+                      type: TextInputType.visiblePassword, obsureText: true),
+                  const SizedBox(
+                    height: 25,
                   ),
-                  () {
-                    // TODO: Get values and signup
-                  },
-                  kLoginSignupSubmitButonStartColor,
-                  kLoginSignupSubmitButonEndColor,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  children: [
+                  AuthCustomSubmitButtons(
                     const Text(
-                      "Dont have an account? ",
+                      "Submit",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                          fontFamily: "SourceSansPro"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignUpScreen()));
-                      },
-                      style: TextButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          alignment: Alignment.centerLeft,
-                          splashFactory: NoSplash.splashFactory),
-                      child: const Text(
-                        "Sign up",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "SourceSansPro",
-                        ),
+                        color: Colors.white,
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ],
-                ),
-              ],
+                    ),
+                    () async {
+                      // TODO: Get values and signup
+                      String email = emailController.text;
+                      String pwd = pwdController.text;
+
+                      var user = await AuthService()
+                          .signInWithEmailAndPassword(email, pwd);
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OverviewScreen(user: user)),
+                          (route) => false);
+                    },
+                    kCeruleanBlue,
+                    kCeruleanBlue,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "Dont have an account? ",
+                        style: TextStyle(
+                            // color: Colors.black,
+                            fontSize: 16.0,
+                            fontFamily: "SourceSansPro"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpScreen()));
+                        },
+                        style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            alignment: Alignment.centerLeft,
+                            splashFactory: NoSplash.splashFactory),
+                        child: const Text(
+                          "Sign up",
+                          style: TextStyle(
+                            color: kSpaceCadet,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "SourceSansPro",
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
