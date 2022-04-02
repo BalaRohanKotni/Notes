09da2b8525152f5941408a67c6a23f0af54b3f27
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-03 01:24:00 +0530

    Updated authCustomTextField.dart

--- lib/components/authCustomTextField.dart
+++ lib/components/authCustomTextField.dart
@@ -4,40 +4,49 @@ import 'package:flutter/material.dart';
 import 'package:notes/constants.dart';
 
 Container AuthCustomTextFeild(
-  helper, {
+  helper,
+  controller, {
   TextInputType type = TextInputType.text,
   bool obsureText = false,
 }) {
   return Container(
-    child: TextField(
-      style: TextStyle(
-        fontSize: 18.0,
-        color: Colors.white,
-      ),
-      obscureText: obsureText,
-      keyboardType: type,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: kLoginSignupTextFeildFillColor,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.0),
-          borderSide: BorderSide(color: Colors.grey, width: 1),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 5),
+          child: Text(
+            helper,
+            style: TextStyle(
+                color: kCaledonBLue,
+                fontFamily: "SourceSansPro",
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.0),
-          borderSide:
-              BorderSide(color: kLoginSignupTextFeildOutlineColor, width: 2.0),
+        TextField(
+          cursorColor: kSpaceCadet,
+          controller: controller,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+          obscureText: obsureText,
+          keyboardType: type,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(color: Colors.grey, width: 2),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(color: Colors.grey, width: 2),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(),
+            ),
+          ),
         ),
-        border: OutlineInputBorder(
-          borderSide: BorderSide(),
-        ),
-        labelText: helper,
-        labelStyle: TextStyle(
-          color: Colors.white,
-          fontFamily: "SourceSansPro",
-          fontSize: 20,
-        ),
-      ),
+      ],
     ),
   );
 }
