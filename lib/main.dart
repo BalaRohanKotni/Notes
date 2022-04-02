1439d53e61d4ba7a67cdebe2772e3de431c97bec
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-03 01:22:00 +0530

    Removed unncessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes/views/login_signupScreen.dart';
-import 'views/overview_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
