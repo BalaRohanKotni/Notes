0219c15d2fb46cfd1dec55ce80560c08b74db9e8
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-06 23:38:14 +0530

    Made changes to the reading of a list in customCard.dart

--- lib/components/customCard.dart
+++ lib/components/customCard.dart
@@ -11,81 +11,102 @@ class CustomCard extends StatelessWidget {
   String body;
   bool darkMode;
   String type;
+  List<dynamic> list;
 
-  CustomCard({
-    Key? key,
-    required this.onTap,
-    required this.creation,
-    required this.updation,
-    required this.title,
-    required this.body,
-    required this.darkMode,
-    required this.type,
-  }) : super(key: key);
+  CustomCard(
+      {Key? key,
+      required this.onTap,
+      required this.creation,
+      required this.updation,
+      required this.title,
+      this.body = "",
+      required this.darkMode,
+      required this.type,
+      this.list = const [
+        {'none': 'none'}
+      ]})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return (type == "note")
-        ? GestureDetector(
-            onTap: onTap(),
-            child: Container(
-              height: 140,
-              padding: EdgeInsets.all(8.0),
-              child: Card(
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                elevation: 10,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: darkMode ? kSpaceCadet : Colors.white,
-                    border: Border.all(color: Colors.grey, width: 0.75),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Text(
-                          title,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: kCeruleanBlue,
-                            fontFamily: "SourceSansPro",
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Text(
-                          body,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: kSpaceCadet,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "SourceSansPro",
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                            " ${creation == updation ? DateFormat("MMMM d y, HH:mm").format((creation.toLocal())) : DateFormat("MMMM d y, HH:mm").format(updation.toLocal())}"),
-                      )
-                    ],
-                  ),
-                ),
+    List<Widget> listItemsTextList = [];
+    if (type == 'todo-list') {
+      for (var todo in list) {
+        if (todo["0"][0] == false) {
+          listItemsTextList.add(Text(todo['0'][1]));
+        } else {
+          listItemsTextList.add(Text(
+            todo['0'][1],
+            style: const TextStyle(decoration: TextDecoration.lineThrough),
+          ));
+        }
+      }
+    }
+    return GestureDetector(
+      onTap: onTap(),
+      child: Container(
+        height: 140,
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: darkMode ? kSpaceCadet : Colors.white,
+              border: Border.all(color: Colors.grey, width: 0.75),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
               ),
             ),
-          )
-        : Container();
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: kCeruleanBlue,
+                      fontFamily: "SourceSansPro",
+                    ),
+                  ),
+                ),
+                Expanded(
+                    flex: 4,
+                    child: (type == 'note')
+                        ? Text(
+                            body,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              color: kSpaceCadet,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "SourceSansPro",
+                            ),
+                          )
+                        : Container(
+                            child: ListView(
+                              shrinkWrap: true,
+                              children: listItemsTextList,
+                            ),
+                          )),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                      " ${creation == updation ? DateFormat("MMMM d y, HH:mm").format((creation.toLocal())) : DateFormat("MMMM d y, HH:mm").format(updation.toLocal())}"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
