42a6d581d959767018d2fb9dcda68ca4eecccdb4
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-14 01:13:54 +0530

    Updated todoList.dart

--- lib/models/todoList.dart
+++ lib/models/todoList.dart
@@ -1,8 +1,8 @@
 class TodoList {
   late final String id;
-  late final DateTime creation;
-  late final DateTime updation;
-  late final String type;
+  late final int creation;
+  late final int updation;
+  static const String type = 'todo-list';
   late final String title;
   late final List<dynamic> list;
 
@@ -10,33 +10,17 @@ class TodoList {
     this.id = "",
     required this.creation,
     required this.updation,
-    required this.type,
     required this.title,
     required this.list,
   });
 
   toMap() {
     return {
-      'creation': creation.millisecondsSinceEpoch,
-      'updation': updation.millisecondsSinceEpoch,
+      'creation': creation,
+      'updation': updation,
       'type': type,
       'title': title,
       'list': list,
     };
-  }
-
-  toStr() {
-    var listOfTodoStr = "";
-    for (var item in list) {
-      item['true'] != null
-          ? listOfTodoStr += "true: ${item['true']}\n"
-          : "nope";
-      item['false'] != null
-          ? listOfTodoStr += "false: ${item['false']}\n"
-          : "nope";
-    }
-
-    print(creation.isUtc);
-    return "ID: $id Creation: $creation, Updation: $updation\nTitle: $title\nTODOS:\n$listOfTodoStr";
   }
 }
