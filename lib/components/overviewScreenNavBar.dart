43db9b98ad0b236f8494aea45278c44fd3bb028c
BalaRohanKotni <balarohan.k@gmail.com>
2022-04-18 18:40:06 +0530

    Updated overviewScreenNavBar.dart

--- lib/components/overviewScreenNavBar.dart
+++ lib/components/overviewScreenNavBar.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
+import '../views/overview_screen.dart';
+
 class OverViewScreenNavBar extends StatelessWidget {
-  const OverViewScreenNavBar({Key? key}) : super(key: key);
+  var user;
+  OverViewScreenNavBar({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        // padding: EdgeInsets.zero,
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => OverviewScreen(
+                            user: user,
+                            type: "all",
+                          )));
+            },
+            child: const Text("All"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => OverviewScreen(
+                            user: user,
+                            type: "note",
+                          )));
+            },
+            child: const Text("Notes"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => OverviewScreen(
+                            user: user,
+                            type: "list",
+                          )));
+            },
+            child: const Text("Lists"),
+          ),
+        ],
+      ),
+    );
   }
 }
