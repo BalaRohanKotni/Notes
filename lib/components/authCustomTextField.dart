d59bc96324cc3805a215437d5d01e7bd05c2eafc
BalaRohanKotni <balarohan.k@gmail.com>
2022-05-07 16:55:39 +0530

    Update authCustomTextField.dart

--- lib/components/authCustomTextField.dart
+++ lib/components/authCustomTextField.dart
@@ -3,50 +3,48 @@
 import 'package:flutter/material.dart';
 import 'package:notes/constants.dart';
 
-Container AuthCustomTextFeild(
+Column authCustomTextFeild(
   helper,
   controller, {
   TextInputType type = TextInputType.text,
   bool obsureText = false,
 }) {
-  return Container(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          margin: EdgeInsets.only(left: 5),
-          child: Text(
-            helper,
-            style: TextStyle(
-                color: kCaledonBLue,
-                fontFamily: "SourceSansPro",
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        TextField(
-          cursorColor: kSpaceCadet,
-          controller: controller,
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        margin: EdgeInsets.only(left: 5),
+        child: Text(
+          helper,
           style: TextStyle(
-            fontSize: 20,
+              color: kCaledonBLue,
+              fontFamily: "SourceSansPro",
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      TextField(
+        cursorColor: kSpaceCadet,
+        controller: controller,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        obscureText: obsureText,
+        keyboardType: type,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(color: Colors.grey, width: 2),
           ),
-          obscureText: obsureText,
-          keyboardType: type,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              borderSide: BorderSide(color: Colors.grey, width: 2),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              borderSide: BorderSide(color: Colors.grey, width: 2),
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(),
-            ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(color: Colors.grey, width: 2),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(),
           ),
         ),
-      ],
-    ),
+      ),
+    ],
   );
 }
