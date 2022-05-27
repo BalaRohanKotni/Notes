ab69107a6cebb539d86a4edbff390b0ecb0b98af
BalaRohanKotni <balarohan.k@gmail.com>
2022-05-28 01:36:43 +0530

    Added const in necessary places in textBlock.dart

--- lib/components/textBlock.dart
+++ lib/components/textBlock.dart
@@ -5,8 +5,8 @@ Expanded textBlock(CustomTextFieldController testController, widget) {
   return Expanded(
     child: Container(
       color: (widget.themeMode == ThemeMode.dark)
-          ? Color.fromARGB(255, 37, 37, 37)
-          : Color.fromARGB(255, 239, 239, 239),
+          ? const Color.fromARGB(255, 37, 37, 37)
+          : const Color.fromARGB(255, 239, 239, 239),
       child: TextField(
           controller: testController,
           maxLines: null,
