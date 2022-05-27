04989dd961c0ce3466a1e34dc5e46d13568c06d0
BalaRohanKotni <balarohan.k@gmail.com>
2022-05-27 20:07:17 +0530

    Updated noteScreen.dart

--- lib/views/noteScreen.dart
+++ lib/views/noteScreen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:notes/components/customTextFieldContoller.dart';
+import 'package:notes/controllers/customTextFieldContoller.dart';
 import 'package:notes/controllers/appTheme.dart';
 
 class NoteScreen extends StatefulWidget {
