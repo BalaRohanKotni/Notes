1fbba7dbdc6b43617732055d9a7b5d924717793d
BalaRohanKotni <balarohan.k@gmail.com>
2022-05-28 16:37:01 +0530

    Added focusNode to textField in textBlock.dart

--- lib/components/textBlock.dart
+++ lib/components/textBlock.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import '../controllers/customTextFieldContoller.dart';
 
-Expanded textBlock(CustomTextFieldController testController, widget) {
+Expanded textBlock(
+    CustomTextFieldController testController, FocusNode focusNode, widget) {
   return Expanded(
     child: Container(
       color: (widget.themeMode == ThemeMode.dark)
           ? const Color.fromARGB(255, 37, 37, 37)
           : const Color.fromARGB(255, 239, 239, 239),
       child: TextField(
+          focusNode: focusNode,
           controller: testController,
           maxLines: null,
           style: const TextStyle(
