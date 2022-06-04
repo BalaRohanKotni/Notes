f0d9e9c2b17760ae5b26a55187ddcd0a7da2be9d
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-04 11:15:44 +0530

    Updated constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,5 @@ const Color kCaledonBLue = Color(0xFF337CA0);
 const Color kCeruleanBlue = Color(0xFF3454D1);
 const Color kSpaceCadet = Color(0xFF3B3561);
 const Color kDarkModeBG = Color(0xFF1A1A1A);
+
+int listBlockId = 0;
