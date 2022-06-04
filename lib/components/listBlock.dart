fbe32f66d102e20d0beaeeca90792d64d084f113
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-04 11:17:34 +0530

    Added listBlock.dart

---
+++ lib/components/listBlock.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:notes/controllers/customTextFieldContoller.dart';
+
+class ListBlock extends StatefulWidget {
+  final List list;
+  final ThemeMode themeMode;
+  final FocusNode focusNode;
+  final VoidCallback emptyListBlock;
+  const ListBlock({
+    Key? key,
+    required this.list,
+    required this.themeMode,
+    required this.focusNode,
+    required this.emptyListBlock,
+  }) : super(key: key);
+
+  @override
+  State<ListBlock> createState() => _ListBlockState();
+}
+
+class _ListBlockState extends State<ListBlock> {
+  List<CustomTextFieldController> controllers = [];
+  List<FocusNode> focusNodes = [];
+  late FocusNode _focusNode;
+  // bool __hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = widget.focusNode;
+    _focusNode.addListener(focusChange);
+    for (var element in widget.list) {
+      controllers.add(CustomTextFieldController());
+      controllers.last.text = element[1];
+      focusNodes.add(FocusNode());
+    }
+  }
+
+  void focusChange() {
+    if (_focusNode.hasFocus) {
+      focusNodes.last.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("rebuilding");
+    focusChange();
+    return Container(
+        color: (widget.themeMode == ThemeMode.dark)
+            ? const Color.fromARGB(255, 37, 37, 37)
+            : const Color.fromARGB(255, 239, 239, 239),
+        child: ReorderableList(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return ListTile(
+              key: ValueKey(index),
+              leading: Checkbox(
+                value: widget.list[index][0],
+                onChanged: (value) {
+                  setState(() {
+                    widget.list[index][0] = value;
+                  });
+                },
+              ),
+              title: RawKeyboardListener(
+                onKey: (event) {
+                  if (event.logicalKey == LogicalKeyboardKey.backspace) {
+                    if (controllers[index].text.isEmpty) {
+                      setState(() {
+                        if (index != 0) {
+                          widget.list.removeAt(index);
+                          controllers.removeAt(index);
+                          focusNodes.removeAt(index);
+                          focusNodes.last.requestFocus();
+                        } else {
+                          widget.emptyListBlock();
+                        }
+                      });
+                    }
+                  }
+                },
+                focusNode: FocusNode(),
+                child: TextField(
+                    focusNode: focusNodes[index],
+                    onSubmitted: (value) {
+                      setState(() {
+                        widget.list.last[1] = controllers.last.text;
+                        widget.list.add([false, ""]);
+                        controllers.add(CustomTextFieldController());
+                        focusNodes.add(FocusNode());
+                        focusNodes.last.requestFocus();
+                      });
+                    },
+                    controller: controllers[index],
+                    maxLines: 1,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    )),
+              ),
+            );
+          },
+          itemCount: widget.list.length,
+          onReorder: (oldIndex, newIndex) {},
+        ));
+    ;
+  }
+}
