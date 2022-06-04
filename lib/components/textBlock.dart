186115e707dee19d20593c04802caeee539249ee
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-04 20:49:46 +0530

    Updated textBlock.dart to not show bg color

--- lib/components/textBlock.dart
+++ lib/components/textBlock.dart
@@ -2,29 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../controllers/customTextFieldContoller.dart';
 
-Container textBlock(CustomTextFieldController textFieldController,
+Widget textBlock(CustomTextFieldController textFieldController,
     FocusNode focusNode, widget, Function deleteTextBlock) {
-  return Container(
-    color: (widget.themeMode == ThemeMode.dark)
-        ? const Color.fromARGB(255, 37, 37, 37)
-        : const Color.fromARGB(255, 239, 239, 239),
-    child: RawKeyboardListener(
-      onKey: (event) {
-        if (textFieldController.text.isEmpty &&
-            event.logicalKey == LogicalKeyboardKey.backspace) deleteTextBlock();
-      },
-      focusNode: FocusNode(),
-      child: TextField(
-          focusNode: focusNode,
-          controller: textFieldController,
-          maxLines: null,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-          ),
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-          )),
-    ),
+  return RawKeyboardListener(
+    onKey: (event) {
+      if (textFieldController.text.isEmpty &&
+          event.logicalKey == LogicalKeyboardKey.backspace) deleteTextBlock();
+    },
+    focusNode: FocusNode(),
+    child: TextField(
+        focusNode: focusNode,
+        controller: textFieldController,
+        maxLines: null,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+        )),
   );
 }
