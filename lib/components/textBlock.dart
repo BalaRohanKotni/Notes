eaabff1e84bf2ded2439c802a9056f9aa6f81143
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-04 16:09:22 +0530

    Added deleteTextBlock function in textBlock.dart

--- lib/components/textBlock.dart
+++ lib/components/textBlock.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../controllers/customTextFieldContoller.dart';
 
-Container textBlock(
-    CustomTextFieldController testController, FocusNode focusNode, widget) {
+Container textBlock(CustomTextFieldController textFieldController,
+    FocusNode focusNode, widget, Function deleteTextBlock) {
   return Container(
     color: (widget.themeMode == ThemeMode.dark)
         ? const Color.fromARGB(255, 37, 37, 37)
         : const Color.fromARGB(255, 239, 239, 239),
-    child: TextField(
-        focusNode: focusNode,
-        controller: testController,
-        maxLines: null,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
-        ),
-        decoration: const InputDecoration(
-          border: InputBorder.none,
-        )),
+    child: RawKeyboardListener(
+      onKey: (event) {
+        if (textFieldController.text.isEmpty &&
+            event.logicalKey == LogicalKeyboardKey.backspace) deleteTextBlock();
+      },
+      focusNode: FocusNode(),
+      child: TextField(
+          focusNode: focusNode,
+          controller: textFieldController,
+          maxLines: null,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+          )),
+    ),
   );
 }
