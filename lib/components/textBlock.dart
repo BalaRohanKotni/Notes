0e742af0bfade9c132c3fa1d2409359a0ea20f8c
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-04 11:15:21 +0530

    Updated textBlock.dart

--- lib/components/textBlock.dart
+++ lib/components/textBlock.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 import '../controllers/customTextFieldContoller.dart';
 
-Expanded textBlock(
+Container textBlock(
     CustomTextFieldController testController, FocusNode focusNode, widget) {
-  return Expanded(
-    child: Container(
-      color: (widget.themeMode == ThemeMode.dark)
-          ? const Color.fromARGB(255, 37, 37, 37)
-          : const Color.fromARGB(255, 239, 239, 239),
-      child: TextField(
-          focusNode: focusNode,
-          controller: testController,
-          maxLines: null,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-          ),
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-          )),
-    ),
+  return Container(
+    color: (widget.themeMode == ThemeMode.dark)
+        ? const Color.fromARGB(255, 37, 37, 37)
+        : const Color.fromARGB(255, 239, 239, 239),
+    child: TextField(
+        focusNode: focusNode,
+        controller: testController,
+        maxLines: null,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+        )),
   );
 }
