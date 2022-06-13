6aefd7d2b19dd7b57905f3e0fbf64c06bf5e1489
BalaRohanKotni <balarohan.k@gmail.com>
2022-06-14 01:04:09 +0530

    Updated main.dart to use firebase_options class for firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:notes/views/signUpScreen.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
