c37f86f604a8a8a4ddf3366031a228fdb7ab9bc4
BalaRohanKotni <balarohan.k@gmail.com>
2022-07-03 22:33:40 +0530

    Removed unused params in customCard.dart

--- lib/components/customCard.dart
+++ lib/components/customCard.dart
@@ -8,7 +8,6 @@ class CustomCard extends StatelessWidget {
   String title;
   dynamic body;
   bool darkMode;
-  String type;
 
   CustomCard({
     Key? key,
@@ -18,32 +17,32 @@ class CustomCard extends StatelessWidget {
     required this.title,
     this.body = "",
     required this.darkMode,
-    required this.type,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> listItemsTextList = [];
-    List<Widget> doneItems = [];
-    List<Widget> notDoneItems = [];
-    if (type == 'list') {
-      for (var todo in body.take(2)) {
-        if (todo["0"][0] == false) {
-          notDoneItems.add(Text(todo['0'][1]));
-        } else {
-          doneItems.add(Text(
-            todo['0'][1],
-            style: const TextStyle(decoration: TextDecoration.lineThrough),
-          ));
-        }
-      }
-      if (body.length > 2) {
-        doneItems.add(const Text(
-          "...",
-        ));
-      }
-      listItemsTextList = notDoneItems + doneItems;
-    }
+    // TODO
+    // List<Widget> listItemsTextList = [];
+    // List<Widget> doneItems = [];
+    // List<Widget> notDoneItems = [];
+    // if (type == 'list') {
+    //   for (var todo in body.take(2)) {
+    //     if (todo["0"][0] == false) {
+    //       notDoneItems.add(Text(todo['0'][1]));
+    //     } else {
+    //       doneItems.add(Text(
+    //         todo['0'][1],
+    //         style: const TextStyle(decoration: TextDecoration.lineThrough),
+    //       ));
+    //     }
+    //   }
+    //   if (body.length > 2) {
+    //     doneItems.add(const Text(
+    //       "...",
+    //     ));
+    //   }
+    //   listItemsTextList = notDoneItems + doneItems;
+    // }
 
     return Card(
       child: GestureDetector(
@@ -70,24 +69,25 @@ class CustomCard extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(
-                  flex: 4,
-                  child: (type == 'note')
-                      ? Text(
-                          body,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "SourceSansPro",
-                          ),
-                        )
-                      : ListView(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          children: listItemsTextList,
-                        )),
+              // TODO
+              // Expanded(
+              //     flex: 4,
+              //     child: (type == 'note')
+              //         ? Text(
+              //             body,
+              //             maxLines: 3,
+              //             overflow: TextOverflow.ellipsis,
+              //             style: const TextStyle(
+              //               fontSize: 14,
+              //               fontWeight: FontWeight.bold,
+              //               fontFamily: "SourceSansPro",
+              //             ),
+              //           )
+              //         : ListView(
+              //             physics: const NeverScrollableScrollPhysics(),
+              //             shrinkWrap: true,
+              //             children: listItemsTextList,
+              //           )),
               Expanded(
                 flex: 2,
                 child: Container(
